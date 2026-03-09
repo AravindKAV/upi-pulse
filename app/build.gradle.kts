d9fb22468bd1c288e0914427e6a24cb65809ba49
AravindKAV <aravindkrishnaav0@gmail.com>
2026-03-09 10:22:36 +0530

    Add Material Components dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
+    
+    implementation("com.google.android.material:material:1.11.0")
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
